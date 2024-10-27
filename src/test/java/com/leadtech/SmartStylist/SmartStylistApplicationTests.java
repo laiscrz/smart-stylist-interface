b0e105580fd38b4d8376680f6af0c2a1542d14a5
laiscrz <lais.albino2003@gmail.com>
2024-10-27 12:31:31 -0300

    feat: Initialize Spring Boot project

---
+++ src/test/java/com/leadtech/SmartStylist/SmartStylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leadtech.SmartStylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
