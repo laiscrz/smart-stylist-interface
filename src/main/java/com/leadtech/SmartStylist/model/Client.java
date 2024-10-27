258af726e1c4014cb59c4cfd088584269eaf7018
laiscrz <lais.albino2003@gmail.com>
2024-10-27 14:16:20 -0300

    feat: created class client

---
+++ src/main/java/com/leadtech/SmartStylist/model/Client.java
@@ -0,0 +1,39 @@
+package com.leadtech.SmartStylist.model;
+
+import com.leadtech.SmartStylist.model.components.Endereco;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.bson.codecs.pojo.annotations.BsonId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.Date;
+import java.util.List;
+
+@Setter
+@Getter
+@Data
+@Document(collection = "client")
+public class Client {
+
+    @Id
+    @BsonId
+    @Field("_id")
+    private String id; // Correspondente ao "_id" do MongoDB
+
+    private String nome;
+    private String email;
+
+    @Field("preferencias_estilo")
+    private List<String> preferenciasEstilo;
+    @Field("data_cadastro")
+    private Date dataCadastro;
+    private int idade;
+    private String genero;
+    private String telefone;
+    private Endereco endereco;
+
+
+}
---
+++ src/main/java/com/leadtech/SmartStylist/model/components/Endereco.java
@@ -0,0 +1,43 @@
+package com.leadtech.SmartStylist.model.components;
+
+import lombok.Data;
+
+@Data
+public class Endereco {
+    private String rua;
+    private String cidade;
+    private String estado;
+    private String pais;
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+}
