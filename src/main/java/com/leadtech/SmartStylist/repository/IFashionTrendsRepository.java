32b3d5aff84264f540a46d72bfefe0f5672947e3
laiscrz <lais.albino2003@gmail.com>
2024-10-30 14:50:39 -0300

    chore: updated classes

--- src/main/java/com/leadtech/SmartStylist/repository/IFashionTrendsRepository.java
+++ src/main/java/com/leadtech/SmartStylist/repository/IFashionTrendsRepository.java
@@ -2,6 +2,8 @@ package com.leadtech.SmartStylist.repository;
 
 import com.leadtech.SmartStylist.model.FashionTrends;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IFashionTrendsRepository extends MongoRepository<FashionTrends, String> {
 }
--- src/main/java/com/leadtech/SmartStylist/repository/IProductRepository.java
+++ src/main/java/com/leadtech/SmartStylist/repository/IProductRepository.java
@@ -2,6 +2,8 @@ package com.leadtech.SmartStylist.repository;
 
 import com.leadtech.SmartStylist.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IProductRepository extends MongoRepository<Product, String> {
 }
